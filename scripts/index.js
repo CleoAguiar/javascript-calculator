f9c6e4b6367e62a7afeffe2a9f8e7cf1b83487ea
Cleo Aguiar <cleotaisas@gmail.com>
2019-05-10 07:54:46 -0300

    feat: user story #2

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,19 @@ class Buttons extends React.Component
     render()
     {
         return (e('div', null,
-                  [e('button', {id: 'equals', value: '=', style: {background: '#ac399'}}, '=')]));
+                  [e('button', {id: 'equals', value: '=', style: {background: '#ac399'}}, '='),
+                   e('button', {id: 'zero', value: '0', onClick:'need improve'}, '0'),
+                   e('button', {id: 'one', value: '1', onClick:'need improve'}, '1'),
+                   e('button', {id: 'two', value: '2', onClick:'need improve'}, '2'),
+                   e('button', {id: 'three', value: '3', onClick:'need improve'}, '3'),
+                   e('button', {id: 'four', value: '4', onClick:'need improve'}, '4'),
+                   e('button', {id: 'five', value: '5', onClick:'need improve'}, '5'),
+                   e('button', {id: 'six', value: '6', onClick:'need improve'}, '6'),
+                   e('button', {id: 'seven', value: '7', onClick:'need improve'}, '7'),
+                   e('button', {id: 'eight', value: '8', onClick:'need improve'}, '8'),
+                   e('button', {id: 'nine', value: '9', onClick:'need improve'}, '9'),
+
+                  ]));
     }
 }
 
