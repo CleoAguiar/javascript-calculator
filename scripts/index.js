5e1d266a55533361b156605743b74d4111f1d3d5
Cleo Aguiar <cleotaisas@gmail.com>
2019-05-17 08:21:57 -0300

    style: remove const operatorBasicStyle and add in css file

--- scripts/index.js
+++ scripts/index.js
@@ -18,8 +18,6 @@ const Footer = () => {
         );
 };
 
-const operatorBasicStyle = {background: '#F80'} ;
-
 
 class Buttons extends React.Component
 {
@@ -27,25 +25,25 @@ class Buttons extends React.Component
     {
         return (e('div', null,
                   [
-                   e('button', {id:'clear', value:'AC', onClick: this.props.initialize }, 'AC'),
+                   e('button', {id:'clear', value:'ac', onClick: this.props.initialize }, 'ac'),
                    e('button', {id:'signal', value:'+/-', onClick: 'need to improve' }, '+/-'),
                    e('button', {id:'percent', value:'%', onClick: 'need to improve' }, '%'),
-                   e('button', {id:'divide', value:'/', onClick: 'need to improve', style: operatorBasicStyle }, '/'),
+                   e('button', {id:'divide', value:'/', onClick: 'need to improve' }, '/'),
                    e('button', {id: 'seven', value: '7', onClick: this.props.number }, '7'),
                    e('button', {id: 'eight', value: '8', onClick: this.props.number }, '8'),
                    e('button', {id: 'nine', value: '9', onClick: this.props.number }, '9'),
-                   e('button', {id:'multiply', value:'*', onClick: 'need to improve', style: operatorBasicStyle }, '*'),
+                   e('button', {id:'multiply', value:'x', onClick: 'need to improve' }, 'x'),
                    e('button', {id: 'four', value: '4', onClick: this.props.number }, '4'),
                    e('button', {id: 'five', value: '5', onClick: this.props.number }, '5'),
                    e('button', {id: 'six', value: '6', onClick: this.props.number }, '6'),
-                   e('button', {id:'subtract', value:'-', onClick: 'need to improve', style: operatorBasicStyle }, '-'),
+                   e('button', {id:'subtract', value:'-', onClick: 'need to improve' }, '-'),
                    e('button', {id: 'one', value: '1', onClick: this.props.number }, '1'),
                    e('button', {id: 'two', value: '2', onClick: this.props.number }, '2'),
                    e('button', {id: 'three', value: '3', onClick: this.props.number }, '3'),
-                   e('button', {id:'add', value:'+', onClick: 'need to improve', style: operatorBasicStyle }, '+'),
+                   e('button', {id:'add', value:'+', onClick: 'need to improve' }, '+'),
                    e('button', {id: 'zero', value: '0', onClick: this.props.number }, '0'),
                    e('button', {id:'decimal', value:'.', onClick: 'need to improve' }, '.'),
-                   e('button', {id: 'equals', value: '=', style: operatorBasicStyle }, '='),
+                   e('button', {id: 'equals', value: '=', onClick: 'need to improve' }, '='),
 
                   ]));
     }
