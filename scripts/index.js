d3eb6226fd391f00a33095e52f93a8e281e90dee
Cleo Aguiar <cleotaisas@gmail.com>
2019-05-27 11:50:58 -0300

    feat: add handleOperator

--- scripts/index.js
+++ scripts/index.js
@@ -27,20 +27,20 @@ class Buttons extends React.Component
                   [
                    e('button', {id:'clear', value:'ac', onClick: this.props.initialize }, 'ac'),
                    e('button', {id:'signal', value:'+/-', onClick: 'need to improve' }, '+/-'),
-                   e('button', {id:'percent', value:'%', onClick: 'need to improve' }, '%'),
-                   e('button', {id:'divide', value:'/', onClick: 'need to improve' }, '/'),
+                   e('button', {id:'percent', value:'%', onClick: this.props.operators }, '%'),
+                   e('button', {id:'divide', value:'/', onClick: this.props.operators }, '/'),
                    e('button', {id: 'seven', value: '7', onClick: this.props.number }, '7'),
                    e('button', {id: 'eight', value: '8', onClick: this.props.number }, '8'),
                    e('button', {id: 'nine', value: '9', onClick: this.props.number }, '9'),
-                   e('button', {id:'multiply', value:'x', onClick: 'need to improve' }, 'x'),
+                   e('button', {id:'multiply', value:'*', onClick: this.props.operators }, 'x'),
                    e('button', {id: 'four', value: '4', onClick: this.props.number }, '4'),
                    e('button', {id: 'five', value: '5', onClick: this.props.number }, '5'),
                    e('button', {id: 'six', value: '6', onClick: this.props.number }, '6'),
-                   e('button', {id:'subtract', value:'-', onClick: 'need to improve' }, '-'),
+                   e('button', {id:'subtract', value:'-', onClick: this.props.operators }, '-'),
                    e('button', {id: 'one', value: '1', onClick: this.props.number }, '1'),
                    e('button', {id: 'two', value: '2', onClick: this.props.number }, '2'),
                    e('button', {id: 'three', value: '3', onClick: this.props.number }, '3'),
-                   e('button', {id:'add', value:'+', onClick: 'need to improve' }, '+'),
+                   e('button', {id:'add', value:'+', onClick: this.props.operators }, '+'),
                    e('button', {id: 'zero', value: '0', onClick: this.props.number }, '0'),
                    e('button', {id:'decimal', value:'.', onClick: 'need to improve' }, '.'),
                    e('button', {id: 'equals', value: '=', onClick: 'need to improve' }, '='),
@@ -70,6 +70,7 @@ class App extends React.Component
         this.handleNumber = this.handleNumber.bind(this);
         this.initialize = this.initialize.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.handleOperator = this.handleOperator.bind(this);
     }
 
     handleNumber(n)
@@ -106,13 +107,19 @@ class App extends React.Component
         this.handleNumber(event.key);
     }
 
+    handleOperator(op)
+    {
+      console.log(op.target.value);
+      // need to improve
+    }
+
 
     render()
     {
         return [e(Header), 
                 e('div', { class: 'calculador'}, [
                   e(Output, { currentValue: this.state.currentVal }),
-                  e(Buttons, { initialize:this.initialize, number: this.handleNumber  })
+                  e(Buttons, { initialize:this.initialize, number: this.handleNumber, operators: this.handleOperator  })
                 ]),
                 e(Footer)];
     }
